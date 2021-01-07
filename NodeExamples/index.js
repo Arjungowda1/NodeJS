0b52ac4e78648262b2250690592d813d192a2909
Arjungowda <Arjunce15@gmail.com>
2021-01-07 10:14:43 +0530

    Simple Node javascripts

---
+++ NodeExamples/index.js
@@ -0,0 +1,24 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b){
+    console.log("Solving for rectangle for l as "+l+" and b as " +b);
+
+    if(l<=0 || b<=0){
+        console.log("Enter values other than zero");
+        console.log(" ")
+    }
+        
+    
+    else{
+        console.log("Perimeter is "+ rect.perimeter(l,b));
+        console.log("Area is "+ rect.area(l,b));
+        console.log(" ")
+    }
+}
+
+solveRect(2,3);
+solveRect(-23,4);
+solveRect(5,4);
